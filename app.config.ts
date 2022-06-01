8db2038a53762b694ff86ae53719a56faf0c95b8
Draculabo <a1842298680@163.com>
2022-06-01 21:51:07 +0800

    chore: initial project

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  title: 'image',
+  description: '基于 Supabase 的图床网站',
+  keywords:
+    'nuxt, naive-ui, supabase, image, web',
+  author: {
+    name: 'draculabo',
+    link: 'https://github.com/draculabo',
+  },
+})
